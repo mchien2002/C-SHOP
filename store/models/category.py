d3141ca246e6850602a9d9bdb456ffd0ca973eaf
hoangminhle208 <hoanganhkks01@gmail.com>
2022-03-25 23:22:39 +0700

    25/03/2022 -Hoang- #16

--- store/models/category.py
+++ store/models/category.py
@@ -5,6 +5,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    @staticmathod
+    def get_all_categories():
+        return Category.objects.all()
+
     # CHIEN: Hiện danh mục sản phẩm ở mục Product/Category
     def __str__(self):
         return self.name
--- store/views.py
+++ store/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models.product import Product
-from store.models import product 
+from store.models import product
+from .models.category import Category
 # Create your views here.
 
 def index(request):
     # CHIEN: tạo list danh sách các product trong database
-    prds = Product.get_all_products()
-    return render(request, 'index.html', {'products': prds})
+    products = Product.get_all_products();
+    categories = Category.get_all_categories()
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request, 'index.html', data)
