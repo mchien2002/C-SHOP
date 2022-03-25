3af3cdae0fa9598682dc5acbe68533d1c9c5a05c
20110262-Nguyễn Minh Chiến <minhchien77777@gmail.com>
2022-03-25 23:39:40 +0700

    fix bug

--- store/models/category.py
+++ store/models/category.py
@@ -5,7 +5,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
-    @staticmathod
+    @staticmethod
     def get_all_categories():
         return Category.objects.all()
 
