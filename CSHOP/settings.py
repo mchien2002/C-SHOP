535b4e8ab521f0f338beced4fd5fbbdafa6ec927
20110262-Nguyễn Minh Chiến <minhchien77777@gmail.com>
2022-04-01 17:00:57 +0700

    add css

--- CSHOP/settings.py
+++ CSHOP/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 #NHUT: tạo đường dẫn đến URL để xứ lý yêu cầu
 MEDIA_URL="/image/download/"
 MEDIA_ROOT='BASE_DIR'
