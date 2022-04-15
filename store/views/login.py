9bf67b62ae1fee162e122955080924226164265c
hoangminhle208 <hoanganhkks01@gmail.com>
2022-04-15 21:13:55 +0700

    15/04/2022 -Hoang- #56

--- store/views/login.py
+++ store/views/login.py
@@ -1,11 +1,13 @@
 from django.views import View 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from store.models.customer import Customer
 from django.contrib.auth.hashers import check_password, make_password
 
 # CHIEN: Class base View
 class Login(View):
+    return_url = None
     def get(self, request):
+        Login.return_url = request.GET.get('return_url')
         return render(request, 'login.html')
 
     def post(self, request):
@@ -17,7 +19,12 @@ class Login(View):
             flag = check_password(password, customer.password)
             if flag:
                 request.session['customer'] = customer.id
-                return redirect('homepage')
+
+                if Login.return_url:
+                    return HttpResponseRedirect(Login.return_url)
+                else:
+                    Login.return_url = None
+                    return redirect('homepage')
             else:
                 error_message = 'Email or Password invalid !!'
         else:
