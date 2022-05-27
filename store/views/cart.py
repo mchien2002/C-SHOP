e194a2629f33175ef4390186b46691a8087b520b
20110262-Nguyễn Minh Chiến <minhchien77777@gmail.com>
2022-05-27 12:55:10 +0700

    fix bug category

--- store/views/cart.py
+++ store/views/cart.py
@@ -1,5 +1,7 @@
+from unicodedata import category
 from django.views import View 
 from django.shortcuts import render, redirect
+from store.models.category import Category
 from store.models.customer import Customer
 from django.contrib.auth.hashers import check_password, make_password
 from store.models.product import Product
@@ -10,4 +12,5 @@ class Cart(View):
     def get(self, request):
         ids=list(request.session.get('cart').keys())
         product_details=ProductDetail.get_productDetails_by_id((ids))
-        return render(request, 'cart.html', {'productDetails':product_details})
+        categories = Category.get_all_categories()
+        return render(request, 'cart.html', {'productDetails':product_details, 'categories': categories})
